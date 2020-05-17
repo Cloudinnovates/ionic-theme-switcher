293ffa281e76209ffbdb1711a0aae253efc3e86c
Marcos Paiva <marcos.paiva@rmcbrothers.com.br>
2020-05-17 17:16:08 -0300

    Initial

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { IonicThemeSwitcherModule } from './ionic-switch-theme.module';
-
-platformBrowserDynamic().bootstrapModule(IonicThemeSwitcherModule)
-  .catch(err => console.log(err));
